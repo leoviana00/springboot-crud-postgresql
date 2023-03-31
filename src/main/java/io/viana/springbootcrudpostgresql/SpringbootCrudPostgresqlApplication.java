5d3f4803ae3205e021d423073c9c9fb167fc7ccd
Leonardo Viana <leonardoviana00@hotmail.com>
2023-03-31 14:38:02 -0300

    feat(spring-crud): basic project springboot

---
+++ src/main/java/io/viana/springbootcrudpostgresql/SpringbootCrudPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package io.viana.springbootcrudpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/viana/springbootcrudpostgresql/SpringbootCrudPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.viana.springbootcrudpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
